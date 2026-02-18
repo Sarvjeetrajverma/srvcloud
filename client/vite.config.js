83720d8db7aee102ee0fba96ea1506b855a821d9
Sarvjeet Raj Verma <153155850+Sarvjeetrajverma@users.noreply.github.com>
2026-02-18 23:42:31 +0530

    ff

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true
-      }
-    }
-  }
-})
+  base: mode === 'production' ? "/my-private-cloud/" : "/",
+}))
