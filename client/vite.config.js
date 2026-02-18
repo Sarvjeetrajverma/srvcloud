025ba22d0197216d39a31bce86c2a08385f29d73
Sarvjeet Raj Verma <153155850+Sarvjeetrajverma@users.noreply.github.com>
2026-02-19 02:09:36 +0530

    bhjc

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,4 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
   base: mode === 'production' ? "/srvcloud/" : "/",
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  }
 }))
